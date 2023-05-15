71f5b42c31e2d63fa5bfe0007b9b30532988d0b4
e1mji <ji7290@icloud.com>
2023-05-16 00:15:16 +0900

    보조제어문 break,continue

---
+++ day02/src/loop/Ex07.java
@@ -0,0 +1,35 @@
+package loop;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		//보조 제어문
+		//break : 제어문을 '즉시' 종료시킨다.
+		for(int i=1; i<=10; i++) {
+			if(i == 5) {
+				break;
+			}
+			System.out.print(i+ "");
+		}
+		System.out.println();
+		
+		//2. continue : 제어문을 '즉시'처음으로
+		for (int i=1; i<=10; i++) {
+			if(i==5) {
+				continue;
+			}
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+	
+	//나만의 해석
+	// for문을통해 지역변수i는 10이 될때까지 증가하며
+	// 종속문장을 실행하기로 되어있다
+	// 여기서 if반복문으로 만약 i가 5가 된다면 
+	// break로 즉시반복문을 빠져나온다.=> 1234
+	
+	//continue는 제어문을 즉시처음으로 되돌리기에 
+	//변수 i는 5가되면 출력문으로 넘어가지않고 다시 처음으로 되돌아가서
+	//조건식을 수행한다. =>1234 678910
+
+}
