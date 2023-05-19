321d248dd01009e47d83a70b559d2e20e842c9a7
e1mji <ji7290@icloud.com>
2023-05-19 12:04:47 +0900

    다양한타입의배열

---
+++ day03/src/array/Ex03.java
@@ -0,0 +1,22 @@
+package array;
+
+import java.util.Arrays;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		//다양한 타입의 배열
+		boolean [] bos = new boolean[4];
+		char[] chs = new char[5];
+		int[] its = new int[2];
+		double[] dbs = new double[3];
+		String[] sts = new String[5];
+		
+		System.out.println("bos = " + Arrays.toString(bos));
+		System.out.println("chs = " + Arrays.toString(chs));
+		System.out.println("its = " + Arrays.toString(its));
+		System.out.println("dbs = " + Arrays.toString(dbs));
+		System.out.println("sts = " + Arrays.toString(sts));
+		
+	}
+
+}
