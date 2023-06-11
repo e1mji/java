a084dcec132d980014f23f1c4b7480feaef81d69
e1mji <ji7290@icloud.com>
2023-06-11 20:11:49 +0900

    커피심부름 계산

---
+++ test/src/day06/Ex02.java
@@ -0,0 +1,41 @@
+package day06;
+
+public class Ex02 {
+	/*
+	 * 차 / 뜨 상관없이 
+		아메리카노 4500
+		카페라떼 5000
+		
+		팀원 마실 메뉴 : 
+		if "카페라테"  = 차가운 카페라떼
+		if "아무거나"  = 차가운 아메리카노
+		
+		결제하게 될 금액 return  int total = 
+	 */
+	
+	public int solution(String[] order) {
+		int americanoPrice = 4500;
+		int cafelattePrice = 5000; 
+        int total = 0;
+
+        for(int i =0; i < order.length; i++) {
+            String menu = order[i];
+        	if (menu != null && menu.equals("anything")) {
+        		total += americanoPrice;
+        	}
+        	else if (menu!= null && menu.contains("americano")) {
+        		total +=americanoPrice;
+        	}
+        	else if (menu != null && menu.contains("cafelatte")) {
+        		total += cafelattePrice;
+        	}
+        }	
+        
+        return total;
+    }
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
