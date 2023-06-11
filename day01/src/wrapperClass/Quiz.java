084203f28137befc44ecc7f6072aa66ca465de4d
e1mji <ji7290@icloud.com>
2023-06-11 20:51:10 +0900

    parse 내장함수를 이용한 간단한 예제

---
+++ day01/src/wrapperClass/Quiz.java
@@ -0,0 +1,33 @@
+package wrapperClass;
+
+import java.util.Scanner;
+
+public class Quiz {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s1, s2;
+		int age;
+		boolean beforeBirth;
+		
+		System.out.print("나이를 정수로 입력하세요 : ");
+		s1 = sc.nextLine();
+		
+		System.out.print("생일이 지났는지 true/false 로 입력하세요 : ");
+		s2 = sc.nextLine();
+		
+		// s1과 s2를 형태에 맞게 변환하여 값을 담아주세요
+		age = Integer.parseInt(s1);
+		beforeBirth = Boolean.parseBoolean(s2);
+		
+		if(beforeBirth) {
+			age = age -1;
+		}
+		else {
+			age = age -2;
+		}
+		System.out.println("당신의 만 나이는 " + age + "살입니다");
+		sc.close();
+	}
+
+}
