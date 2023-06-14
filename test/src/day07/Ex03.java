a4ac561c6497d99fbcd169f6d336a257edf6a3f6
e1mji <ji7290@icloud.com>
2023-06-14 23:18:19 +0900

    업&다운

---
+++ test/src/day07/Ex03.java
@@ -0,0 +1,37 @@
+package day07;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ex03 {
+	public static void main(String[] args) {
+		//랜덤을 활용한 업다운 게임을 시작하세요
+		Scanner sc = new Scanner(System.in);
+		Random ran = new Random();
+		int answer = ran.nextInt(100)+1;
+		int user;
+		int cnt = 0;
+		
+		//while문은 true인동안 반복
+		while(true) {
+			System.out.print("정수입력 (1~100) : ");
+			user = sc.nextInt();
+			cnt ++;
+			//만약 유저가 입력한 값이 랜덤값보다 높으면 down출력
+			if(user> answer) {
+				System.out.println("DOWN");
+			}
+			// 유저가 입력한 값이  랜덤 값보다 낮으면 up
+			else if(user < answer){
+				System.out.println("UP");
+			}
+			// 맞추면 break;
+			else {
+				break;
+			}
+		}	//정답입니다.
+			System.out.printf("정답입니다. %d회 만에 정답을 맞췄습니다.\n",cnt);
+		
+	}
+
+}
